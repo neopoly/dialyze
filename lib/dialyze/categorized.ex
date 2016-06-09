629daf49b8804ca0fcc33e5fd7e97752c0998f7d
Peter Suschlik <peter@suschlik.de>
2016-06-09 20:21:03 +0200

    Cast warnings into a struct

--- lib/dialyze/categorized.ex
+++ lib/dialyze/categorized.ex
@@ -1,8 +1,22 @@
 defmodule Dialyze.Formatter.Categorized do
+  defmodule Warning do
+    defstruct [:category, :filename, :line, :metadata]
+  end
+
   def print_warnings(warnings) do
     warnings
+    |> Enum.map(&cast_warning/1)
     |> Enum.each(&format_warning/1)
     :ok
+  end
+
+  defp cast_warning({category, {filename, line}, metadata}) do
+    %Warning{
+      category: category,
+      filename: filename |> to_string,
+      line: line,
+      metadata: metadata
+    }
   end
 
   defp format_warning(warning) do
